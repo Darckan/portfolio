c8bfbdcec8be66f4e7f973f3b4061ed6bb641f70
Darckan <remushurmuz4@gmail.com>
2025-07-28 16:38:42 +0200

    addapt to kubernetes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,6 @@ import { existsSync } from 'fs';
 import * as dotenv from 'dotenv';
 
 async function bootstrap(): Promise<void> {
-  if (!process.env.IS_DOCKER && existsSync('.env')) {
-    dotenv.config({ path: '.env' });
-  }
-
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
